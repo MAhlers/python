655a71538c5544e49623168eabace97f2ad3e847
Michael S. Ahlers <michael.s.ahlers@gmail.com>
2016-04-10 23:04:25 -0400

    Updated __main__.

--- HelloWorld/__main__.py
+++ HelloWorld/__main__.py
@@ -2,6 +2,7 @@
 
 import os
 import model
+import sys
 import utilities
 
 class HelloWorld:
@@ -10,7 +11,7 @@ class HelloWorld:
 	project_root = os.path.dirname(os.path.abspath(__file__))
 	
 	db = (project_root + '\data\db.txt')
-	#empty_db = (project_root + 'data/emptydb.txt')
+	#empty_db = (project_root + '\data\emptydb.txt')
 	
 	data_size = os.stat(db).st_size
 
@@ -33,6 +34,8 @@ class HelloWorld:
 					print('Data_File Contains ', row_count, ' Records.')
 				
 				model.close_connection(conn)
+				
+				sys.stdin.readline()
 				
 			except OSError as e:
 
